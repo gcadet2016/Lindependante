045456bf7d9541003a88c798694fbb1017b171d9
Guillaume Cadet <cadet.guillaume@outlook.fr>
2024-02-27 10:41:18 +0100

    Update deployment script

---
+++ tools/deployApache.sh
@@ -0,0 +1,4 @@
+cp -r /mnt/c/Users/guigu/OneDrive/Dev/Esecad/Phase1/2-Front-end/5-Concepts_base_CSS/lindependante/* /var/www/html
+rm -rf /var/www/html/.git
+rm -rf /var/www/html/docker -
+rm -rf /var/www/html/tools
